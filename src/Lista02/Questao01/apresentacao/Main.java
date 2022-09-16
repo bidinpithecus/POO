7f1415a1bd875b1f734c605b3e51c0de5c99adee
Administrador <vinibidin@gmail.com>
2022-09-16 10:08:05 -0300

    Lista 02 - Exercício 01 continuação

--- src/Lista02/Questao01/apresentacao/Main.java
+++ src/Lista02/Questao01/apresentacao/Main.java
@@ -33,6 +33,9 @@ public class Main {
         pessoa.setNome(input.nextLine());
         System.out.println("Idade:");
         pessoa.setIdade(input.nextInt());
+        if (pessoa.getIdade() <= 0) {
+            return null;
+        }
         System.out.println("Cpf:");
         pessoa.setCpf(input.nextLine());
         System.out.println("Cidade:");
@@ -43,10 +46,37 @@ public class Main {
 
     public static void adicionarPessoa(Pessoa pessoa) {
         int idade = pessoa.getIdade();
-        if (idade >= 1 && idade <= 12) {
-            pessoas.put("Criancas", pessoa);
+        if (pessoa.equals(null)) {
+            return;
+        } else if (idade >= 1 && idade <= 12) {
+            pessoas.put("Criancas", add(pessoa));
             // add person to list on the set
+        } else if (idade >= 13 && idade <= 18) {
+            pessoas.put("Adolescentes", add(pessoa));
+            // add person to list on the set
+        } else if (idade >= 19 && idade <= 25) {
+        pessoas.put("Jovens", add(pessoa));
+        // add person to list on the set
+        } else if (idade >= 26 && idade <= 59) {
+        pessoas.put("Adultos", add(pessoa));
+        // add person to list on the set
+        } else if (idade >= 60) {
+        pessoas.put("Idosos", add(pessoa));
+        // add person to list on the set
         }
+    }
+
+    public static void main(String[] args) {
+        int opcao = 0;
+        while (true) {
+            opcao = input.nextInt();
+            if (opcao == -1) {
+                return;
+            }
+            adicionarPessoa(novaPessoa());
+        }
+
+
     }
 
 }

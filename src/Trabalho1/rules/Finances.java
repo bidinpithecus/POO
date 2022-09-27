f67b5b6871a8e52344b25245852fafb1ab8d2434
13189385971 <13189385971@edu.udesc.br>
2022-09-27 18:59:40 -0300

    Some methods in Finances made

--- src/Trabalho1/rules/Finances.java
+++ src/Trabalho1/rules/Finances.java
@@ -4,32 +4,72 @@ import Trabalho1.data.Category;
 import Trabalho1.data.Spent;
 import Trabalho1.data.User;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Calendar;
+import java.util.*;
 
 public class Finances {
     private Map<User, List<Spent>> users = new HashMap<>();
+    private User logged;
 
     public boolean newUser(User user) {
-        return true;
+        if (users.get(user) == null) {
+            List<Spent> spents = new ArrayList<>();
+            users.put(user, spents);
+            return true;
+        }
+        return false;
     }
 
     // https://www.baeldung.com/java-password-hashing
     public boolean login(String login, String password) {
-        return true;
+        for (User user : users.keySet()) {
+            if (user.getLogin().equals(login) && user.getPassword().equals(password)) {
+                logged = user;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void logoff() {
+        logged = new User();
+    }
+
+    public boolean isUserLogged() {
+        return logged != null;
     }
 
     public boolean newSpent(Spent spent) {
-        return true;
+        if (isUserLogged()) {
+            users.get(logged).add(spent);
+            return true;
+        }
+        return false;
     }
 
-    public boolean deleteSpent(String id) {
-        return true;
+    public boolean deleteSpent(int index) {
+        if (isUserLogged()) {
+            users.get(logged).remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Spent> listSpent() {
+        return users.get(logged);
     }
 
     public List<Spent> listSpent(int month) {
+        if (isUserLogged()) {
+            List<Spent> list = new ArrayList<>();
+            for (Spent spent : users.get(logged)) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTime(spent.getDate().getTime());
+                if ((calendar.get(Calendar.MONTH) + 1) == month) {
+                    list.add(spent);
+                }
+            }
+            return list;
+        }
         return null;
     }
 

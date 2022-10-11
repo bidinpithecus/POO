621c984a3c3dc426644d8d42dd7446426e3e4711
bidinpithecus <vinibidin@gmail.com>
2022-10-11 18:43:07 -0300

    some changes

--- src/AulaPratica07/dados/Gerador.java
+++ src/AulaPratica07/dados/Gerador.java
@@ -7,6 +7,7 @@ import java.util.Random;
 
 public abstract class Gerador implements ISequencia {
     protected List<Integer> sequencia = new ArrayList<>();
+    private final Random random = new Random();
 
     public abstract void gerar(int quantidade);
 
@@ -16,8 +17,7 @@ public abstract class Gerador implements ISequencia {
 
     public int sortear() {
         // utilize random
-        Random gerador = new Random();
-        return sequencia.get(gerador.nextInt(sequencia.size() - 1));
+        return sequencia.get(random.nextInt(sequencia.size() - 1));
     }
 
     public long somatorio() {
@@ -37,7 +37,7 @@ public abstract class Gerador implements ISequencia {
         for (Integer num : sequencia) {
             product *= num;
         }
-        return (double) Math.pow(product, 1.0 / sequencia.size());
+        return Math.pow(product, 1.0 / sequencia.size());
     }
 
     public double variancia() {
@@ -47,7 +47,7 @@ public abstract class Gerador implements ISequencia {
         for (Integer num : sequencia) {
             variance += Math.pow(num - arithAvg, 2);
         }
-        return (double) variance / sequencia.size() - 1;
+        return variance / sequencia.size() - 1;
     }
 
     public double desvioPadrao() {
@@ -55,6 +55,6 @@ public abstract class Gerador implements ISequencia {
     }
 
     public long amplitude() {
-        return Collections.max(sequencia) - Collections.min(sequencia);
+        return (long) Collections.max(sequencia) - Collections.min(sequencia);
     }
 }
